66f6b4a03f916e8684a2ea0135421f7bb3ea212d
miyayamaM <pendulum.ngt.4010@gmail.com>
2023-01-22 20:35:45 +0900

    feat: apply fizzbuzz to 1 - 100

--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,8 @@ use crate::number_converter::NumberConverter;
 
 fn main() {
     let fizz_buzz = NumberConverter {};
-    fizz_buzz.convert(1);
+    for i in 1..100 {
+        let converted_string = fizz_buzz.convert(i);
+        println!("{}", converted_string)
+    }
 }
