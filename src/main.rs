1a96472c4e52f2e6b7625227a6077eac072099d5
miyayamaM <pendulum.ngt.4010@gmail.com>
2023-02-02 09:07:20 +0900

    refactor: remove create_printer method

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,8 @@
 pub mod factories;
 
 use crate::factories::fizz_buzz_app_factory::FizzBuzzAppFactory;
-use clean_fizz_buzz::number_converter::NumberConverter;
-use clean_fizz_buzz::outputs::console_output::ConsoleOutput;
-use clean_fizz_buzz::printer::FizzBuzzSequencePrinter;
-
-pub fn create_printer() -> FizzBuzzSequencePrinter<NumberConverter, ConsoleOutput> {
-    let factory = FizzBuzzAppFactory {};
-    factory.create()
-}
-
 fn main() {
-    let printer = create_printer();
+    let factory = FizzBuzzAppFactory {};
+    let printer = factory.create();
     printer.print_range(0, 100)
 }
